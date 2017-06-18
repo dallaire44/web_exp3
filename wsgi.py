a4faf42bdfdc19f56bd2abc11f8ea7cad6d56bba
dallaire44 <david.dallaire@sympatico.ca>
2017-06-17 20:51:24 -0400

    second

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify,json,request
+
 application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+@application.route('/')
+def showQualificationsList():
+    return render_template('list.html')
 
 if __name__ == "__main__":
     application.run()
